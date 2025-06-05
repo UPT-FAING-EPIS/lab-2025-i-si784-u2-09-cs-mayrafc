375e46cd3c8e5069de1daf3130edcbd65fdb6904
mayrafc <mc2021072620@virtual.upt.pe>
2025-06-05 17:04:49 -0500

    lab9

---
+++ Bank/Bank.Domain.Tests/Features/BanckAccountTests.cs
@@ -0,0 +1,83 @@
+using Bank.Domain;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace Bank.Domain.Tests.Features
+{
+    [Binding]
+    public sealed class CuentaAhorroPruebas
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private CuentaAhorro? _cuenta;
+        private string? _error;
+        private bool _esError;
+
+        public CuentaAhorroPruebas(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            _esError = false;
+        }
+
+        [Given("la nueva cuenta numero (.*)")]
+        public void DadoUnaNuevaCuenta(string numeroCuenta)
+        {
+            try
+            {
+                var cliente = Cliente.Registrar("Juan Perez");
+                _cuenta = CuentaAhorro.Aperturar(numeroCuenta, cliente, 1);
+            }
+            catch (System.Exception ex)
+            {
+                _esError = true;
+                _error = ex.Message;
+            }
+        }
+
+        [Given("con saldo (.*)")]
+        [When("deposito (.*)")]
+        public void CuandoYoDeposito(decimal monto)
+        {
+            try
+            {
+                _cuenta?.Depositar(monto);
+            }
+            catch (System.Exception ex)
+            {
+                _esError = true;
+                _error = ex.Message;
+            }
+        }
+
+        [When("retiro (.*)")]
+        public void CuandoYoRetiro(decimal monto)
+        {
+            try
+            {
+                _cuenta?.Retirar(monto);
+            }
+            catch (System.Exception ex)
+            {
+                _esError = true;
+                _error = ex.Message;
+            }
+        }
+
+        [Then("el saldo nuevo deberia ser (.*)")]
+        public void EntoncesElSaldoNuevoDeberiaSer(decimal resultado)
+        {
+            Assert.That(_cuenta?.Saldo, Is.EqualTo(resultado));
+        }
+
+        [Then("deberia ser error")]
+        public void EntoncesDeberiaSerError()
+        {
+            Assert.That(_esError, Is.True);
+        }
+
+        [Then("deberia mostrarse el error: (.*)")]
+        public void EntoncesDeberiaMostrarseElError(string error)
+        {
+            Assert.That(_error, Is.EqualTo(error));
+        }
+    }
+}
